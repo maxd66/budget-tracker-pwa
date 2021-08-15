f7f301619c56d4b23392921b70b577e9bc526b79
maxd66 <max.dunn63@gmail.com>
2021-08-14 21:12:34 -0700

    created function to save offline additions to indexedBDB

--- public/db.js
+++ public/db.js
@@ -4,13 +4,31 @@
 function saveRecord(record) {
   // TODO: this function should save a transaction object to indexedDB so that
   // it can be synced with the database when the user goes back online.
+  const request = window.indexedDB.open("transactions", 1);
+
+  request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+
+    const store = db.createObjectStore("transactions", {
+      keyPath: "date",
+    });
+  };
+
+  request.onsuccess = () => {
+    const db = request.result;
+    const tx = db.transaction(["transactions"], "readwrite");
+    const txStore = tx.objectStore("transactions");
+
+    txStore.add(record);
+  };
 }
 
 function checkDatabase() {
   // TODO: this function should check for any saved transactions and post them
   // all to the database. Delete the transactions from IndexedDB if the post
   // request is successful.
+  const request = window.indexedDB.open("transactions", 1);
 }
 
 // listen for app coming back online
-window.addEventListener('online', checkDatabase);
+window.addEventListener("online", checkDatabase);
