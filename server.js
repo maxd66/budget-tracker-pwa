1fad5cacae429de09abf5e56237c7b7c2946c845
maxd66 <max.dunn63@gmail.com>
2021-08-14 22:56:50 -0700

    added route for /

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget_pwa", {
 // routes
 app.use(require("./routes/api.js"));
 
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
